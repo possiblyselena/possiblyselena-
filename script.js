4c154e74dd90ba1ff30ba0d974145bdc9eeaa94c
Selena (Possibly) <149027636+possiblyselena@users.noreply.github.com>
2024-06-21 02:06:50 +0000

    Cafe-ified

---
+++ script.js
@@ -0,0 +1,24 @@
+function icecoffee(){
+  document.getElementById("creations").innerHTML = " ";
+    document.getElementById("creations").innerHTML = "ice coffee";
+  document.getElementById("description").innerHTML = " ";
+    document.getElementById("description").innerHTML = "there should be a decription about my instagram here";
+}
+function lavenderlatte(){
+  document.getElementById("creations").innerHTML = " ";
+    document.getElementById("creations").innerHTML = "lavender lattes";
+  document.getElementById("description").innerHTML = " ";
+    document.getElementById("description").innerHTML = "there should be a decription about my linkedin here"
+}
+function yuzutea(){
+  document.getElementById("creations").innerHTML = " ";
+    document.getElementById("creations").innerHTML = "yuzu tea";
+  document.getElementById("description").innerHTML = " ";
+    document.getElementById("description").innerHTML = "there should be a decription about my youtube here"
+}
+function rosetea(){
+  document.getElementById("creations").innerHTML = " ";
+    document.getElementById("creations").innerHTML = "rose buds";
+  document.getElementById("description").innerHTML = " ";
+    document.getElementById("description").innerHTML = "there should be a decription about my robotics experience here"
+}
