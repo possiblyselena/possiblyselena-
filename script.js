13bcc825ce6387bc40bd2c684a72a3f843f9cd8f
Selena <149027636+possiblyselena@users.noreply.github.com>
2024-06-29 22:07:55 +0000

    added recipes to images

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ function cookies(){
   document.getElementById("creations").innerHTML = " ";
     document.getElementById("creations").innerHTML = "cookies";
   document.getElementById("description").innerHTML = " ";
-    document.getElementById("description").innerHTML = "insert recipe here";
+    document.getElementById("description").innerHTML = "my go to recipe for cookies";
 }
 function brownies(){
   document.getElementById("creations").innerHTML = " ";
     document.getElementById("creations").innerHTML = "brownies";
   document.getElementById("description").innerHTML = " ";
-    document.getElementById("description").innerHTML = "insert recipe here"
+    document.getElementById("description").innerHTML = "my go to recipe for brownies";
 }
 function cakes(){
   document.getElementById("creations").innerHTML = " ";
     document.getElementById("creations").innerHTML = "cakes";
   document.getElementById("description").innerHTML = " ";
-    document.getElementById("description").innerHTML = "insert recipe here";
+    document.getElementById("description").innerHTML = "i used this recipe for my birthday cake!";
 }
 function rosetea(){
   document.getElementById("creations").innerHTML = " ";
